da0a58b697aa822b7bab7ce0794ab6df7f435814
sofib <sbogert@usc.edu>
2022-06-28 23:18:02 -0700

    more refactoring

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
     return (
-        <div>
+        <div class="big-papa">
             <nav class="navbar-bkgd">
                 <ul class="nav-list">
                     <div class="hey-world">
