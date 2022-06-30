398d8ea03e2eb5ca4bf5440fd0c0382ba164ac21
sofib <sbogert@usc.edu>
2022-06-30 02:18:02 -0700

    nice color set 2

--- src/App.js
+++ src/App.js
@@ -4,30 +4,28 @@ import './App.css';
 function App() {
     return (
         <div class="big-papa">
-            <nav class="navbar-box">
-                <ul class="nav-list">
-                    <div class="hey-world">
-                        <h2 class="special-h2">Sophia Bogert</h2>
-                    </div>
-                    <li><a href="#projects">Projects + Xp</a></li>
-                    <li><a href="#fun">Fun Things</a></li>
-                    <li><a href="#contact">Contact Me!</a></li>
-                </ul>
-            </nav>
+        <nav class="navbar-box">
+            <ul class="nav-list">
+                
+                <li><a href="#projects">Projects + Xp</a></li>
+                <li><a href="#fun">Fun Things</a></li>
+                <li><a href="#contact">Contact Me!</a></li>
+            </ul>
+        </nav>
 
-            <div class="box-main">
-                <h1 class="text-big">Hola Mundo</h1>
-                <p class="text-small">
-                    Hello World, my name is Sofi Bogert
-                </p>
-            </div>
-    
-            <footer class="footer">
-                <p class="text-footer">
-                    * Made with Love *
-                </p>
-            </footer>
+        <div class="box-main">
+            <h1 class="text-big">Hola Mundo</h1>
+            <p class="text-small">
+                Hello World, my name is Sofi Bogert
+            </p>
         </div>
+
+        <footer class="footer">
+            <p class="text-footer">
+                * Made with Love *
+            </p>
+        </footer>
+    </div>
     )
 }
 export default App
