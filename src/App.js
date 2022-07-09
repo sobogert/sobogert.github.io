937fe4461a90cff664a3708b5f2b458b7fef7943
sofib <sbogert@usc.edu>
2022-07-08 19:07:05 -0700

    oops

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div class="big-papa">
+        <nav class="navbar-box">
+            <ul class="nav-list">
+                
+                <li><a href="#projects">Projects + Xp</a></li>
+                <li><a href="#fun">Fun Things</a></li>
+                <li><a href="#contact">Contact Me!</a></li>
+            </ul>
+        </nav>
+
+        <div class="box-main">
+            <h1 class="text-big">Hola Mundo</h1>
+            <p class="text-small">
+                Hello World, my name is Sofi Bogert
+            </p>
+        </div>
+
+        <footer class="footer">
+            <p class="text-footer">
+                * Made with Love *
+            </p>
+        </footer>
+    </div>
+    )
+}
+export default App
